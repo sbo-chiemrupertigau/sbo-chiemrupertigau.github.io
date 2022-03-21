7afc7b0cbdba3d02496d1761b8c3be1bff25146b
Sebastian Geschke <sebastian.geschke@gmail.com>
2022-03-21 20:19:31 +0100

    deploy website

---
+++ assets/1961df8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{459:function(n,t,e){"use strict";e.r(t);var o={name:"App",components:{About:function(){return e.e(2).then(e.bind(null,460))}}},l=e(21),component=Object(l.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("about")}),[],!1,null,null,null);t.default=component.exports}}]);
