434b082d9695e8e26701d3d9530af5e6ba36c399
Sebastian Geschke <sebastian.geschke@magnolia-cms.com>
2025-02-06 21:14:37 +0100

    deploy website

--- assets/e230358.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{464:function(n,e,t){"use strict";t.r(e);var l={name:"App",components:{Timeline:function(){return t.e(3).then(t.bind(null,460))}}},c=t(21),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("timeline")}),[],!1,null,null,null);e.default=component.exports}}]);
